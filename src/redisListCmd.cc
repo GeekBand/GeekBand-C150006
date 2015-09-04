1e04261f5a300c11e1843451a3dbab3b12e50481
dingcongjun <dingcongjun@gmail.com>
2015-09-04 13:57:49 +0800

    提交rpush和rpop的代码，冗余较多，后期整改

--- src/redisListCmd.cc
+++ src/redisListCmd.cc
@@ -88,8 +88,106 @@ ResponsePtr LpopCmd::process(const std::vector<RequestParam>& cmdParam)
     return ResponsePtr(new BulkResponse(StrObjectPtr()));
   }
 
+  if (objPtr->typeNmae() != "list")
+  {
+    return ResponsePtr(new ErrResponse("WRONGTYPE", "Operation against a key holding the wrong kind of value"));
+  }
+
   ListObjectPtr listPtr = boost::static_pointer_cast<ListObject>(objPtr);
   StrObjectPtr item = boost::static_pointer_cast<StrObject>(listPtr->lpop());
+  if (listPtr->llen() == 0)
+  {
+    instance->deleteKeyValue(key);
+  }
+
+  return BulkResponsePtr(new BulkResponse(item));
+}
+
+///////////////////////// cmd of rpush begin ///////////////////////////////
+std::string RpushCmd::name_("RPUSH");
+RpushCmd RpushCmd::prototype_;
+
+RpushCmd::RpushCmd()
+{
+  Cmd::addPrototype(name_, this);
+}
+
+RpushCmd::RpushCmd(const std::string& name)
+{
+  (void)name;
+}
+
+ResponsePtr RpushCmd::process(const std::vector<RequestParam>& cmdParam)
+{
+  if (cmdParam.size() <= 2)
+  {
+    return ResponsePtr(new ErrResponse("ERR", "wrong number of arguments for 'lpush' command"));
+  }
+
+  std::string key(cmdParam[1].start(), cmdParam[1].len());
+  DatabaseManage *instance = DatabaseManage::getInstance();
+  ObjectPtr objPtr = instance->queryKeyValue(key);
+
+  if (objPtr.get() && objPtr->typeNmae() != "list")
+  {
+    return ResponsePtr(new ErrResponse("WRONGTYPE", "Operation against a key holding the wrong kind of value"));
+  }
+
+  ListObjectPtr listPtr = objPtr.get() ? boost::static_pointer_cast<ListObject>(objPtr)
+                                       : ListObjectPtr(new ListObject());
+
+  for (size_t i = 2; i < cmdParam.size(); i++)
+  {
+    StrObjectPtr item(new StrObject(cmdParam[i].start(), cmdParam[i].len()));
+    listPtr->rpush(item);
+  }
+
+  if (objPtr.get() == NULL)
+  {
+    instance->updateKeyValue(key, listPtr);
+  }
+
+
+  return IntResponsePtr(new IntResponse(listPtr->llen()));
+}
+
+///////////////////////// cmd of lpop begin ///////////////////////////////
+std::string RpopCmd::name_("RPOP");
+RpopCmd RpopCmd::prototype_;
+
+RpopCmd::RpopCmd()
+{
+  Cmd::addPrototype(name_, this);
+}
+
+RpopCmd::RpopCmd(const std::string& name)
+{
+  (void)name;
+}
+
+ResponsePtr RpopCmd::process(const std::vector<RequestParam>& cmdParam)
+{
+  if (cmdParam.size() != 2)
+  {
+    return ResponsePtr(new ErrResponse("ERR", "wrong number of arguments for 'lpop' command"));
+  }
+
+  std::string key(cmdParam[1].start(), cmdParam[1].len());
+  DatabaseManage *instance = DatabaseManage::getInstance();
+  ObjectPtr objPtr = instance->queryKeyValue(key);
+
+  if (!objPtr.get())
+  {
+    return ResponsePtr(new BulkResponse(StrObjectPtr()));
+  }
+
+  if (objPtr->typeNmae() != "list")
+  {
+    return ResponsePtr(new ErrResponse("WRONGTYPE", "Operation against a key holding the wrong kind of value"));
+  }
+
+  ListObjectPtr listPtr = boost::static_pointer_cast<ListObject>(objPtr);
+  StrObjectPtr item = boost::static_pointer_cast<StrObject>(listPtr->rpop());
   if (listPtr->llen() == 0)
   {
     instance->deleteKeyValue(key);
--- src/redisListCmd.h
+++ src/redisListCmd.h
@@ -37,6 +37,36 @@ class LpopCmd: public Cmd
   static LpopCmd prototype_;
 };
 
+//////////////////////////// cmd of lpush start /////////////////////////////
+class RpushCmd: public Cmd
+{
+ public:
+  Cmd *clone() const { return new RpushCmd(name_); }
+  ResponsePtr process(const std::vector<RequestParam>& cmdParam);
+  const std::string& typeName() const { return name_; }
+ private:
+  RpushCmd();
+  RpushCmd(const std::string& name);
+
+  static std::string name_;
+  static RpushCmd prototype_;
+};
+
+//////////////////////////// cmd of lpop start /////////////////////////////
+class RpopCmd: public Cmd
+{
+ public:
+  Cmd *clone() const { return new RpopCmd(name_); }
+  ResponsePtr process(const std::vector<RequestParam>& cmdParam);
+  const std::string& typeName() const { return name_; }
+ private:
+  RpopCmd();
+  RpopCmd(const std::string& name);
+
+  static std::string name_;
+  static RpopCmd prototype_;
+};
+
 }
 
 #endif
